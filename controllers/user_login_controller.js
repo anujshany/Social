e0c0c264988a4ace3286cb57a65e76ecd29de40f
anujshany <anujshany@gmail.com>
2021-07-11 07:42:33 +0530

    added logout button succesfully

--- controllers/user_login_controller.js
+++ controllers/user_login_controller.js
@@ -1,5 +1,21 @@
 const User = require('../models/user')
 
+module.exports.profile = function(req, res){
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user',{
+                    title:"User Profile",
+                    user: user
+                })
+            }
+            return res.redirect('/user/sign-in')
+        })
+    }else{
+        return res.redirect('/users/sign-in')
+    }
+ }
+ 
 //render the sign up page
 module.exports.signUp = function(req,res){
     return res.render('user_sign_up',{
@@ -38,5 +54,31 @@ module.exports.create = function(req,res){
 //sign in and create a session for the user
 
 module.exports.createSession = function(req,res){
-    //TODO Later
+    //steps to authenticate
+    // find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('error in finding user in signing in');return}
+        // handle user found
+        if(user){
+
+            // handle incorrect passwords
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            // handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile')
+
+        }else{
+            // handle incorrect passwords
+            return res.redirect('back');
+        }
+
+    })};
+
+module.exports.logout = function(req,res){
+        res.cookie('user_id', null)
+        return res.render('user_sign_in',{
+            title:"WeCode | Sign In"
+        })
 }
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const usersController = require('../controllers/users_controller');
 const usersPosts = require('../controllers/user_posts');
 const userLogin = require('../controllers/user_login_controller');
 
-router.get('/profile', usersController.profile);
+router.get('/profile',userLogin.profile);
 router.get('/posts', usersPosts.post);
 router.get('/sign-up', userLogin.signUp);
 router.get('/sign-in',userLogin.signIn);
-router.post('/create',userLogin.create)
+router.post('/create',userLogin.create);
+router.post('/create-session',userLogin.createSession);
+router.post('/logout',userLogin.logout);
 
 module.exports = router;
