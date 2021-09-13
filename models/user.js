84a5980394b8243eee08248064f4957716e0ba3e
anujshany <anujshany@gmail.com>
2021-09-13 16:23:29 +0530

    Friendship Schema Prepared

--- models/user.js
+++ models/user.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
     },
     avatar:{
         type: String
+    },
+    friendship:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Friendship'
     }
 }, {
     timestamps: true
